674805126bda078d98c289507a6d030a2f2167d6
Rifaut Alexis <ralexis149@gmail.com>
2024-09-12 19:14:20 +0200

    add a layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NzLayoutModule, NzMenuModule, NgOptimizedImage],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
 })
-export class AppComponent {
-  title = 'sparkers';
-}
+export class AppComponent { }
